c7b9fa59ef9509fcf47e5e2ca592b0ed64c3bc32
alilaode <itman.ali.laode@gmail.com>
2023-10-16 10:52:54 +0700

    feat/product: add repositoires

---
+++ Services/Product/Product.Core/Entities/Medicine.cs
@@ -0,0 +1,16 @@
+﻿using Product.Core.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Product.Core.Entities
+{
+    public class Medicine : EntityBase
+    {
+        public string? Name { get; set; }
+        public decimal? Price { get; set; }
+        
+    }
+}
---
+++ Services/Product/Product.Core/Repositories/IProductRepository.cs
@@ -0,0 +1,18 @@
+﻿using Product.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Product.Core.Repositories
+{
+
+    public interface IProductRepository
+    {
+        Task<Medicine> GetMedicine(string name);
+        Task<bool> CreateMedicine(Medicine medicine);
+        Task<bool> UpdateMedicine(Medicine medicine);
+        Task<bool> DeleteMedicine(Medicine name);
+    }
+}
